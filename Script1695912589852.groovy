8f251e777b6ff9906b47fb3f5dc549ed826f7e60
Cahya12345 <146356054+Cahya12345@users.noreply.github.com>
2023-09-28 23:24:47 +0700

    Add files via upload

---
+++ Script1695912589852.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+String username = 'Admin'
+String password = 'admin123'
+String url = 'https://opensource-demo.orangehrmlive.com/web/index.php/leave/applyLeave'
+
+WebUI.openBrowser(url)
+WebUI.maximizeWindow()
+WebUI.setText(findTestObject('Login/input_Username_username'), username)
+WebUI.setText(findTestObject('Login/input_Password_password'), password)
+WebUI.click(findTestObject('Login/button_Login'))
+WebUI.verifyElementPresent(findTestObject('Login/div_Dashboard'), 0, FailureHandling.STOP_ON_FAILURE)
+WebUI.takeScreenshot()
+
+
